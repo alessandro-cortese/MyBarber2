43797c3d5832ef5b39abeae2926df2ed6ddd40f2
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2021-11-10 11:57:48 +0100

    Update

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("hello-view.fxml")));
-        FXMLLoader root = new FXMLLoader(getClass().getClassLoader().getResource("com/example/demo/hello-view.fxml")) ;
+        FXMLLoader root = new FXMLLoader(getClass().getClassLoader().getResource("com/example/demo/mobile_login_2.fxml")) ;
         Scene scene = new Scene(root.load());
         stage.setTitle("Prova");
         stage.setScene(scene);
