05b82478bd96544dee225959e58234a9e5d76c79
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2021-11-10 20:00:05 +0100

    Switch

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
+    private FXMLLoader root;
+    private Stage stage;
+    private Scene scene;
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void switchToRegister(ActionEvent event) throws Exception{
+        root = new FXMLLoader(getClass().getClassLoader().getResource("com/example/demo/register_screen.fxml")) ;
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
