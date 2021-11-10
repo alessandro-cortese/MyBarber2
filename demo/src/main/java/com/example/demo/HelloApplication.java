8a53b8926446c6458201ac8ab27a6be8b860adba
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2021-11-10 11:29:46 +0100

    Commit

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("com.example.demo.hello-view.fxml"));
-        Scene scene = new Scene(new Group() , 500, 500);
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root, 500, 500);
         stage.setTitle("Prova");
         stage.setScene(scene);
         stage.show();
