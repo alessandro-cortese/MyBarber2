1eb61c2ffefddac1a153c9e11ef0b770440568ec
EdmondDantes <snicosanti@gmail.com>
2021-11-10 11:54:00 +0100

    Prova

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -8,15 +8,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(root, 500, 500);
+        //FXMLLoader root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("hello-view.fxml")));
+        FXMLLoader root = new FXMLLoader(getClass().getClassLoader().getResource("com/example/demo/hello-view.fxml")) ;
+        Scene scene = new Scene(root.load());
         stage.setTitle("Prova");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
