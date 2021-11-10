05c457d60f9aef1a000af8d74e7a67a750040dbf
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2021-11-10 11:28:22 +0100

    Prova

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("com.example.demo.hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("com.example.demo.hello-view.fxml"));
         Scene scene = new Scene(new Group() , 500, 500);
         stage.setTitle("Prova");
         stage.setScene(scene);
