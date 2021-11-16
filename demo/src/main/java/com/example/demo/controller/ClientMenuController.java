4bf1f2f547d305f390cddf6c855ff9d5d8a198cf
EdmondDantes <snicosanti@gmail.com>
2021-11-16 15:59:42 +0100

    Aggiunta Compra Prodotto

--- demo/src/main/java/com/example/demo/controller/ClientMenuController.java
+++ demo/src/main/java/com/example/demo/controller/ClientMenuController.java
@@ -10,15 +10,17 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class ClientMenuController {
 
     private final String SEE_APPOINTMENTS_SCREEN_NAME = "com/example/demo/client_see_appointments.fxml" ;
+    private final String BUY_PRODUCT_SCREEN_NAME = "com/example/demo/buy_product.fxml";
     private final String CLIENT_HOME_SCREEN_NAME = "com/example/demo/client_home.fxml" ;
+
     private final String CLIENT_APPOINTMENT_ITEM = "com/example/demo/client_see_appointments_list_item.fxml" ;
+    private final String BUY_PRODUCT_ITEM = "com/example/demo/buy_product_list_item.fxml";
 
     @FXML
     private MenuBar clientMenuBar ;
@@ -41,18 +43,29 @@ public class ClientMenuController {
     public void onMenuItemSelected(ActionEvent event) throws IOException {
         MenuItem sourceItem = (MenuItem) event.getSource() ;
         BorderPane clientBorderPane = (BorderPane) clientMenuBar.getScene().getRoot();
+        ListView listView ;
+        Parent newCenterNode = null ;
         if ("seeAppointmentsItem".equals(sourceItem.getId())) {
-            Parent seeAppointmentsNode = (new FXMLLoader(getClass().getClassLoader().getResource(SEE_APPOINTMENTS_SCREEN_NAME))).load();
-            ListView appointmentsListView = (ListView) seeAppointmentsNode.lookup("#appointmentsListView");
-
-            Node[] nodesList = new Node[10] ;
-            for (int i = 0 ; i < 10 ; i++) {
-                nodesList[i] = (new FXMLLoader(getClass().getClassLoader().getResource(CLIENT_APPOINTMENT_ITEM))).load() ;
-            }
-            ClientAppointmentsList clientAppointmentsList = new ClientAppointmentsList(nodesList);
-            appointmentsListView.setItems(clientAppointmentsList);
-
-            clientBorderPane.setCenter(seeAppointmentsNode);
+            newCenterNode = (new FXMLLoader(getClass().getClassLoader().getResource(SEE_APPOINTMENTS_SCREEN_NAME))).load();
+            listView = (ListView) newCenterNode.lookup("#appointmentsListView");
+            onLoadListItems(listView, CLIENT_APPOINTMENT_ITEM);
         }
+
+        else if ("buyProductItem".equals(sourceItem.getId())) {
+            newCenterNode = (new FXMLLoader(getClass().getClassLoader().getResource(BUY_PRODUCT_SCREEN_NAME))).load();
+            listView = (ListView) newCenterNode.lookup("#buyProductListView");
+            onLoadListItems(listView, BUY_PRODUCT_ITEM);
+        }
+
+        clientBorderPane.setCenter(newCenterNode);
+    }
+
+    private void onLoadListItems(ListView listView, String itemResource) throws IOException {
+        Node[] nodesList = new Node[10] ;
+        for (int i = 0 ; i < 10 ; i++) {
+            nodesList[i] = (new FXMLLoader(getClass().getClassLoader().getResource(itemResource))).load() ;
+        }
+        ClientAppointmentsList clientAppointmentsList = new ClientAppointmentsList(nodesList);
+        listView.setItems(clientAppointmentsList);
     }
 }
