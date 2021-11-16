807b8df48eaf3134ee0484da3a911071158f66a6
Roberto <roberto.fard@gmail.com>
2021-11-16 16:43:29 +0100

    Create clienthomeController.java

---
+++ demo/src/main/java/com/example/demo/controller/clienthomeController.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button ButtonAppointmentBooked;
+
+    @FXML
+    private Button ButtonBuyProduct;
+
+    @FXML
+    private Button ButtonFidelityCard;
+
+}
