a70f5fd590b40ba4f20b5b31eae1fa9e3f9fa5d6
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2021-11-19 08:08:36 +0100

    Aggiunto lo scheduler del Barbiere

--- demo/src/main/java/com/example/demo/controller/ScheduleTimeBarberController.java
+++ demo/src/main/java/com/example/demo/controller/ScheduleTimeBarberController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import TimePicker.TimePicker;
+<<<<<<< Updated upstream
 import com.example.demo.OtherBarberScheduler.SchedulerRowController;
+=======
+import eu.hansolo.tilesfx.Test;
+>>>>>>> Stashed changes
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,11 +16,60 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
 
 public class ScheduleTimeBarberController {
+
+    private CheckBox[] arrayCheckBox;
+
+    {
+        extractedCheckBox();
+    }
+
+    private void extractedCheckBox() {
+        arrayCheckBox = new CheckBox[]{
+                mondayMorningCheckBox,
+                mondayAfternoonCheckBox,
+                dayTwoMorningCheckBox,
+                dayTwoAfternoonCheckBox,
+                dayThreeMorningCheckBox,
+                dayThreeAfternoonCheckBox,
+                dayFourMorningCheckBox,
+                dayFourAfternoonCheckBox,
+                dayFiveMorningCheckBox,
+                dayFiveAfternoonCheckBox,
+                daySixMorningCheckBox,
+                daySixAfternoonCheckBox
+        };
+    }
+
+    private TextField[][] arrayTextFiled;
+
+    {
+        extractedTextField();
+    }
+
+    private void extractedTextField(){
+
+        arrayTextFiled = new TextField[][]{
+                {mondayMorningOpen, mondayMorningClose },
+                {mondayAfternoonOpen, mondayAfternoonClose},
+                {dayTwoMorningOpen, dayTwoMorningClose },
+                {dayTwoAfternoonOpen, dayTwoAfternoonClose},
+                {dayThreeMorningOpen, dayThreeMorningClose},
+                {dayThreeAfternoonOpen, dayThreeAfternoonClose},
+                {dayFourMorningOpen, dayFourMorningClose},
+                {dayFourAfternoonOpen, dayFourAfternoonClose},
+                {dayFiveMorningOpen, dayFiveMorningClose},
+                {dayFiveAfternoonOpen, dayFiveAfternoonClose},
+                {daySixMorningOpen, daySixMorningClose},
+                {daySixAfternoonOpen, daySixAfternoonClose}
+        };
+    }
+
 
     @FXML
     private CheckBox mondayMorningCheckBox;
@@ -25,14 +78,95 @@ public class ScheduleTimeBarberController {
     private CheckBox mondayAfternoonCheckBox;
 
     @FXML
+    private CheckBox dayTwoMorningCheckBox;
+
+    @FXML
+    private CheckBox dayTwoAfternoonCheckBox;
+
+    @FXML
+    private CheckBox dayThreeMorningCheckBox;
+
+    @FXML
+    private CheckBox dayThreeAfternoonCheckBox;
+
+    @FXML
+    private CheckBox dayFourAfternoonCheckBox;
+
+    @FXML
+    private CheckBox dayFourMorningCheckBox;
+
+    @FXML
+    private CheckBox dayFiveMorningCheckBox;
+
+    @FXML
+    private CheckBox dayFiveAfternoonCheckBox;
+
+    @FXML
+    private CheckBox daySixMorningCheckBox;
+
+    @FXML
+    private CheckBox daySixAfternoonCheckBox;
+
+
+    @FXML
     private TextField mondayMorningOpen;
     @FXML
     private TextField mondayMorningClose;
+
     @FXML
     private TextField mondayAfternoonOpen;
     @FXML
     private TextField mondayAfternoonClose;
 
+    @FXML
+    private TextField dayTwoMorningOpen;
+    @FXML
+    private TextField dayTwoMorningClose;
+
+    @FXML
+    private TextField dayTwoAfternoonOpen;
+    @FXML
+    private TextField dayTwoAfternoonClose;
+
+    @FXML
+    private TextField dayThreeMorningOpen;
+    @FXML
+    private TextField dayThreeMorningClose;
+
+    @FXML
+    private TextField dayThreeAfternoonOpen;
+    @FXML
+    private TextField dayThreeAfternoonClose;
+
+    @FXML
+    private TextField dayFourMorningOpen;
+    @FXML
+    private TextField dayFourMorningClose;
+
+    @FXML
+    private TextField dayFourAfternoonOpen;
+    @FXML
+    private TextField dayFourAfternoonClose;
+
+    @FXML
+    private TextField dayFiveMorningOpen;
+    @FXML
+    private TextField dayFiveMorningClose;
+
+    @FXML
+    private TextField dayFiveAfternoonOpen;
+    @FXML
+    private TextField dayFiveAfternoonClose;
+
+    @FXML
+    private TextField daySixMorningOpen;
+    @FXML
+    private TextField daySixMorningClose;
+
+    @FXML
+    private TextField daySixAfternoonOpen;
+    @FXML
+    private TextField daySixAfternoonClose;
 
     @FXML
     public void onButtonClicked(ActionEvent event) throws IOException {
@@ -51,46 +185,65 @@ public class ScheduleTimeBarberController {
     @FXML
     public void onCheckSelected(ActionEvent event){
         CheckBox localCheckBox = (CheckBox) event.getSource();
-        if(localCheckBox == mondayMorningCheckBox){
-            if(mondayMorningCheckBox.isSelected()){
+        this.extractedCheckBox();
+        this.extractedTextField();
+        int i = 0;
+        boolean flag = true;
+        int index = 0;
 
-                mondayMorningOpen.setVisible(false);
-                mondayMorningClose.setVisible(false);
-
-            }else{
-
-                mondayMorningOpen.setVisible(true);
-                mondayMorningClose.setVisible(true);
-
-            }
-        }else if(localCheckBox == mondayAfternoonCheckBox){
-            if(mondayAfternoonCheckBox.isSelected()){
-
-                mondayAfternoonOpen.setVisible(false);
-                mondayAfternoonClose.setVisible(false);
-
-            }else{
-
-                mondayAfternoonOpen.setVisible(true);
-                mondayAfternoonClose.setVisible(true);
-
+        for(i = 0; i < arrayCheckBox.length && flag; i++) {
+            if(localCheckBox == arrayCheckBox[i]) {
+                index = i;
+                System.out.println(index);
+                flag = false;
             }
         }
 
-    }
+        if(arrayCheckBox[index].isSelected()){
 
-    private void cheBoxSelected(CheckBox checkBox) {
+            arrayTextFiled[index][0].setVisible(false);
+            arrayTextFiled[index][1].setVisible(false);
 
+        }else{
 
+            arrayTextFiled[index][0].setVisible(true);
+            arrayTextFiled[index][1].setVisible(true);
 
+        }
 
     }
 
     @FXML
     public void onChangeTime(MouseEvent event) throws IOException {
-        ((TextField) event.getSource()).setText((new TimePicker(0,12)).getTime());
-    }
+        TextField textField = (TextField) event.getSource();
+        this.extractedTextField();
+        int i = 0;
+        int j = 0;
+        int rawIndex = 0;
+        int columnIndex = 0;
+        boolean flag = true;
 
+        for(i = 0; i < arrayTextFiled.length && flag; i++) {
+            for(j = 0; j < arrayTextFiled[i].length && flag; j++) {
+                if(textField == arrayTextFiled[i][j]){
+                    rawIndex = i;
+                    columnIndex = j;
+                    flag = false;
+                }
+            }
+        }
+
+       if(rawIndex % 2 == 0){
+
+           ((TextField) event.getSource()).setText((new TimePicker(7, 13)).getTime());
+
+       }else{
+
+           ((TextField) event.getSource()).setText((new TimePicker(14, 21)).getTime());
+
+       }
+
+    }
 
 
 }
