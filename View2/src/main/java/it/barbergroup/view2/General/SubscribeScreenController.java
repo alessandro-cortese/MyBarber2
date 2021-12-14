605ccdb2de89ebf39e07197be188bc273a02ddd3
EdmondDantes <snicosanti@gmail.com>
2021-12-14 15:46:12 +0100

    Sistemata SubscribeScreen

--- View2/src/main/java/it/barbergroup/view2/General/SubscribeScreenController.java
+++ View2/src/main/java/it/barbergroup/view2/General/SubscribeScreenController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.Map;
+
 public class SubscribeScreenController {
 
     private static final String[] subscribeCommands = {
@@ -12,7 +14,7 @@ public class SubscribeScreenController {
             "set email",
             "set address",
             "set password",
-            "set type",
+            "type",
             "register",
             "google register",
             "facebook register",
@@ -28,44 +30,71 @@ public class SubscribeScreenController {
     @FXML TextField userTypeField ;
 
 
+    private Map<String, TextField> textFieldMap ;
+
+    private void initMap() {
+        textFieldMap = Map.of(
+                "set name", nameField,
+                "set surname", surnameField,
+                "set email", subscribeEmailField,
+                "set address", addressField,
+                "set password", subscribePasswordField,
+                "set type", userTypeField) ;
+    }
+
 
     @FXML
     public void onCommand() {
         String commandText = subscribeCommandLine.getText();
         subscribeCommandLine.setStyle(null);
         subscribeCommandLine.setText("");
+        initMap();
 
-        if (commandText.startsWith(subscribeCommands[0])) {
-            nameField.setText(commandText.replace(subscribeCommands[0] + " ", "" ));
-        }
-        else if (commandText.startsWith(subscribeCommands[1])) {
-            surnameField.setText(commandText.replace(subscribeCommands[1] + " ", "" ));
-        }
-        else if (commandText.startsWith(subscribeCommands[2])) {
-            subscribeEmailField.setText(commandText.replace(subscribeCommands[2] + " ", "" ));
-        }
-        else if (commandText.startsWith(subscribeCommands[3])){
-            addressField.setText(commandText.replace(subscribeCommands[3] + " ", "" ));
-        }
-        else if (commandText.startsWith(subscribeCommands[4])) {
-            subscribePasswordField.setText(commandText.replace(subscribeCommands[4] + " ", "" ));
-        }
-        else if (commandText.startsWith(subscribeCommands[5])) {
-            String userType = commandText.replace(subscribeCommands[5] + " ", "") ;
-            if (userType.compareTo("B") == 0) {
-                userTypeField.setText("Barber");
+
+        if (commandText.startsWith("set")) {
+            for (Map.Entry<String, TextField> entry : textFieldMap.entrySet()) {
+                String command = entry.getKey();
+                if (commandText.startsWith(command)) {
+                    String input = commandText.replace(command + " ", "") ;
+                    if(input.compareTo(command) != 0) {
+                        entry.getValue().setText(input);
+                        return ;
+                    }
+                }
             }
-            else if (userType.compareTo("C") == 0) {
-                userTypeField.setText("Client");
+
+        }
+        else if (commandText.startsWith("type")) {
+            String input = commandText.replace("type ", "") ;
+            if (input.compareTo("B") == 0 || input.compareTo("C") == 0) {
+                userTypeField.setText(input);
+                return ;
+            }
+
+        }
+        else if (commandText.startsWith("register")) {
+            String output = "";
+            if (commandText.compareTo("register") == 0) {
+                output = "normalRegister" ;
+            }
+            else if (commandText.compareTo("register google") == 0) {
+                output = "googleRegister" ;
+            }
+            else if (commandText.compareTo("register facebook") == 0) {
+                output = "facebookRegister" ;
             }
             else {
                 subscribeCommandLine.setStyle("-fx-border-color: red");
+                return ;
             }
+            System.out.println(output);
+            return;
         }
 
-        else {
-            subscribeCommandLine.setStyle("-fx-border-color: red");
-        }
+        subscribeCommandLine.setStyle("-fx-border-color: red");
+
 
     }
+
+
 }
