bb813c058082ccaa5bc9c8212593e7810baef6f4
Roberto <roberto.fard@gmail.com>
2021-12-28 10:59:53 +0100

    f

--- src/main/java/engineering/pattern/Connector.java
+++ src/main/java/engineering/pattern/Connector.java
@@ -11,7 +11,7 @@ public class Connector { //SINGLETON CLASS
             //if (System.getenv("RDS_HOSTNAME") != null) {
                 try {
                     System.out.println("Loading driver...");
-                    Class.forName("com.mysql.jdbc.Driver");
+                    Class.forName("com.mysql.cj.jdbc.Driver");
                     System.out.println("Driver loaded!");
                     /*String dbName = System.getenv("RDS_DB_NAME");
                     String userName = System.getenv("RDS_USERNAME");
