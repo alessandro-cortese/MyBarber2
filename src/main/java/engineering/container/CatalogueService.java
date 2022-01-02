4f8865b11bf21185ed299e1564a14b393f7031a1
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2022-01-02 16:18:57 +0100

    Aggiunta catalogo servizi

--- src/main/java/engineering/container/CatalogueService.java
+++ src/main/java/engineering/container/CatalogueService.java
@@ -1,6 +1,7 @@
 package engineering.container;
 
 import model.Service;
+import model.buyProduct.Product;
 
 import java.util.List;
 
@@ -10,8 +11,31 @@ public class CatalogueService {
     public CatalogueService(List<Service> service){
         this.service=service;
     }
+
     public CatalogueService(){
         this.service=null;
+    }
+
+    public Service createService(String name, String description, double price, Product usedProduct) {
+         return new Service(name, description, price, usedProduct);
+    }
+
+    public void addService(Service newService) {
+        service.add(newService);
+    }
+
+    public void addService(String name, String description, double price, Product usedProduct) {
+        Service localService = new Service(name, description, price, usedProduct);
+        service.add(localService);
+    }
+
+    public Service getServiceByName (String serviceName) {
+        for(Service service : service) {
+            if(service.getName().compareTo(serviceName) == 0) {
+                return service;
+            }
+        }
+        return null;
     }
 
 }
