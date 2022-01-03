e47e5cf21840dc7b6159e41c59f43cb3b36869d0
Cortix24 <74416238+Cortix24@users.noreply.github.com>
2022-01-03 18:17:25 +0100

    Summury

--- src/main/java/model/Saloon.java
+++ src/main/java/model/Saloon.java
@@ -3,6 +3,7 @@ package model;
 import java.sql.Time;
 
 public class Saloon {
+
     private String name;
     private String city;
     private String address;
@@ -11,51 +12,87 @@ public class Saloon {
     private int seatNumber;
 
     public Saloon(String name, String city, String address, String phone,Time slotTime, int seatNumber){
-        this.name=name;
-        this.address=address;
-        this.phone=phone;
-        this.city=city;
+
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.city = city;
         this.seatNumber = seatNumber;
         this.slotTime = slotTime;
 
     }
 
     public String getName(){
+
         return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getCity(){
-        return city;
-    }
-    public void setCity(String city){
-        this.city = city;
-    }
-    public String getAddress(){
-        return address;
-    }
-    public  void  setAddress(String address){
-        this.address = address;
-    }
-    public String getPhone(){
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public Time getSlotTime(){
-        return slotTime;
-    }
-    public void setSlotTime(Time slotTime) {
-        this.slotTime = slotTime;
+
     }
 
+    public void setName(String name){
+
+        this.name=name;
+
+    }
+
+    public String getCity(){
+
+        return city;
+
+    }
+
+    public void setCity(String city){
+
+        this.city = city;
+
+    }
+
+    public String getAddress(){
+
+        return address;
+
+    }
+
+    public  void  setAddress(String address){
+
+        this.address = address;
+
+    }
+
+    public String getPhone(){
+
+        return phone;
+
+    }
+
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+
+    }
+
+    public Time getSlotTime(){
+
+        return slotTime;
+    }
+
+
+    public void setSlotTime(Time slotTime) {
+
+        this.slotTime = slotTime;
+
+    }
+
+
     public int getSeatNumber() {
+
         return seatNumber;
+
     }
 
     public void setSeatNumber(int seatNumber) {
+
         this.seatNumber = seatNumber;
+
     }
+
 }
